8270d78ff3916e1e399cef52e83f833f9bb73923
jiradeto <jiradet.jd@gmail.com>
2017-04-10 13:36:53 +0700

    add form

--- src/app/components/user.component.ts
+++ src/app/components/user.component.ts
@@ -3,15 +3,43 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'user',
   template: ` 
-  <h1>Hello {{name}}</h1>
+  <h1>{{name}}</h1>
   <p> Email: {{email}}</p>
   <p> Address: {{address.street}} {{address.city}}, {{address.country}}</p>
+  <button (click)="toggleHobbies()">{{showHobbies ? "Hide Hobbies": "Show Hobbies"}}</button> 
+  <div *ngIf="showHobbies">
+    <h3> Hobbies </h3>
+    <ul>
+      <li *ngFor="let hobby of hobbies; let i = index">
+        {{hobby}} <button (click)="deleteHobby(i)">X</button>
+      </li>
+    </ul>
+    <form (submit)="addHobby(hobby.value)"> 
+      <label> Add Hobby: </label><br />
+      <input type="text" #hobby /><br />
+    </form>
+  </div>
+
+  <form>
+    <label>Name: </label><br />
+    <input type="text" name="name" [(ngModel)]= "name" /><br/>
+    <label>Email: </label><br />
+    <input type="text" name="email" [(ngModel)]= "email" /><br/>
+    <label>Street: </label><br />
+    <input type="text" name="street" [(ngModel)]= "address.street" /><br/>
+    <label>City: </label><br />
+    <input type="text" name="city" [(ngModel)]= "address.city" /><br/>
+    <label>Street: </label><br />
+    <input type="text" name="country" [(ngModel)]= "address.country" /><br/>
+  </form>
   `
 })
 export class UserComponent {
   name: string;
   email: string;
   address: address;
+  hobbies: string[];
+  showHobbies: boolean;
 
   constructor() {
     this.name = 'John Doe';
@@ -21,6 +49,18 @@ export class UserComponent {
       city: 'Bangkok',
       country: 'Thailand'
     }
+    this.hobbies = ['Music', 'Movies', 'Sports']
+    this.showHobbies = false;
+  }
+
+  toggleHobbies(){
+    this.showHobbies = !this.showHobbies;
+  }
+  addHobby(hobby){
+    this.hobbies.push(hobby);
+  }
+  deleteHobby(i){
+    this.hobbies.splice(i,1)
   }
 }
 
